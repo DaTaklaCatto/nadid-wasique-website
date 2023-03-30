2116af894f761a15cd597396f049cfd2c3bff501
Nadid Wasique <76746289+DaTaklaCatto@users.noreply.github.com>
2023-03-30 12:48:40 +0600

    Added Hero Text Hover Animation Effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,35 @@
   const onscroll = (el, listener) => {
     el.addEventListener("scroll", listener);
   };
+//Hero Text Animation
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("#ani_text").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
 
   // Navbar Active Design Function
   let navbarlinks = select("#navbar .scrollto", true);
